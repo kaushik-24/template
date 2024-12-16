00ff08ac89ad7113e1c33f97cbff6c8db88d11c3
kaushik <helworld6@gmail.com>
2024-12-16 12:32:05 +0545

    update

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// Select the left content and the right sidebar
-const leftContent = document.getElementById("leftContent");
-const rightSidebar = document.getElementById("rightSidebar");
+const leftContent = document.getElementById("left-content");
+const rightSidebar = document.getElementById("right-side-bar");
 
-// Get the bounding rectangle of the left content
 const updateSidebarPosition = () => {
   const leftContentRect = leftContent.getBoundingClientRect();
 
   if (leftContentRect.bottom <= window.innerHeight) {
-    // If the left content finishes, make the sidebar relative
     rightSidebar.classList.add("relative");
   } else {
-    // If the left content is still scrolling, make the sidebar fixed
     rightSidebar.classList.remove("relative");
   }
 };
 
-// Listen for the scroll event
 window.addEventListener("scroll", updateSidebarPosition);
